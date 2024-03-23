d7944c11a4cba8853453fa2759f5e1b38557a8c6
Your Name <you@example.com>
2024-03-23 13:54:12 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -134,7 +134,7 @@ int parentesisBalanceados(char *cadena)
             else
             {
               if(*c == '}' && *c2 == '{') pop(P);
-              else return 0;
+              else return 1;
             }
           }
         }
